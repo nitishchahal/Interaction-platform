b50a08c674170fd6bb4f28e2224a9f81848cbab3
nitishchahal <nitishchoudharyc2@gmail.com>
2025-08-12 12:17:06 +0530

    Change in Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,31 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App';
-// import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-// // Import Toastify
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+// Import Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// createRoot(document.getElementById('root')).render(
-//   <>
-//     <App />
-//     <ToastContainer position="top-right" autoClose={2000} />
-//   </>
-// );
-
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { AuthProvider } from "./context/AuthContext";
-
-createRoot(document.getElementById("root")).render(
-  <AuthProvider>
+createRoot(document.getElementById('root')).render(
+  <>
     <App />
     <ToastContainer position="top-right" autoClose={2000} />
-  </AuthProvider>
+  </>
 );
+
+
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { AuthProvider } from "./context/AuthContext";
+// createRoot(document.getElementById("root")).render(
+//   <AuthProvider>
+//     <App />
+//     <ToastContainer position="top-right" autoClose={2000} />
+//   </AuthProvider>
+// );
 
