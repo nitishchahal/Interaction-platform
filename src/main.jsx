2dc2d05fd27fad20931027286caa4a3d25e01cb2
nitishchahal <nitishchoudharyc2@gmail.com>
2025-08-12 12:17:47 +0530

    Change in Main

--- src/main.jsx
+++ src/main.jsx
@@ -15,17 +15,3 @@ createRoot(document.getElementById('root')).render(
 );
 
 
-// import React from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { AuthProvider } from "./context/AuthContext";
-// createRoot(document.getElementById("root")).render(
-//   <AuthProvider>
-//     <App />
-//     <ToastContainer position="top-right" autoClose={2000} />
-//   </AuthProvider>
-// );
-
